3925818e514e8bd3fa58a1d90aa2b8945f5063e2
Andre Luis Oliveira <algo1305@gmail.com>
2017-04-25 20:27:35 -0300

    Creating views
    Creating basic views to link pages.
    
    Finalized example app.

--- env/helloapp/howdy/views.py
+++ env/helloapp/howdy/views.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
+#howdy/views.py
+
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
+
+
+class AboutPageView(TemplateView):
+    template_name="about.html"
